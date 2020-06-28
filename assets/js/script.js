47b16dfee00ea1cfaccca5f8c36732e4731ba760
Leo Ayala <leo_a1016@yahoo.com>
2020-06-28 00:38:37 -0400

    added funtionality to the delete button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+var pageContentEl = document.querySelector("#page-content")
 var taskIdCounter = 0;
 
 var formEl = document.querySelector("#task-form");
@@ -91,3 +92,18 @@ var createTaskActions = function(taskId) {
 };
 
 formEl.addEventListener("submit", taskFormHandler);
+
+var taskButtonHandler = function(event) {
+
+    if (event.target.matches(".delete-btn")) {
+           // get the element's task id
+    var taskId = event.target.getAttribute("data-task-id");
+    deleteTask(taskId);
+    }
+  };
+  var deleteTask = function(taskId) {
+    var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+    taskSelected.remove();
+};
+
+pageContentEl.addEventListener("click", taskButtonHandler)
